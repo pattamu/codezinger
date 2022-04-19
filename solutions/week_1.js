99c6091528225b8d1c4ffdb3f03a9b99c0fd9078
pattamu <102967813+pattamu@users.noreply.github.com>
2022-04-19 21:09:14 +0530

    2 problems in week_1 added

--- solutions/week_1.js
+++ solutions/week_1.js
@@ -34,6 +34,7 @@ Delete from array if multiple of 5
 //     if(arr[i] % 5 !== 0) result.push(arr[i])
 // }
 // console.log("New Array: ",result)
+// console.log(arr.filter(x => x%5 != 0)) // alt method
 
 /*
 Find prime numbers in an array
@@ -69,4 +70,35 @@ Find sum of first and last digit of a number
 // sum += Math.floor(num)
 // console.log('The Sum of the first and last  digits are: ', sum) 
 
-/***********************************************************************************************/
+/*
+Inversions in an Array
+*/
+
+// const arr = prompt('Enter elemets for the Array with spaces: ').split(' ')
+// console.log("The Array is: ",arr)
+// let inv_count = 0
+// for(let i=0; i<arr.length-1; i++){
+//     for(let j=i+1; j<arr.length; j++){
+//         if(Number(arr[i]) > Number(arr[j]))
+//         inv_count++
+//     }
+// }
+// console.log("The number of inversions is: ",inv_count)
+
+/*
+Palindrom array Check
+*/
+
+// const arr = prompt('Enter elemets for the Array with spaces: ').split(' ')
+// console.log("The Array is: ",arr)
+// let flag = false; 
+// for(let i=0; i<arr.length/2; i++){
+//     if(arr[i] != arr[arr.length-1-i]){
+//         flag = true
+//         break
+//     }
+// }
+// if(flag == true) console.log("Not a Palindrom") 
+// console.log("A Palindrom Array") 
+
+/**********************************************************************/
